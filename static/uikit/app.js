458becb984b9022aa1b81efa973bc15b5b426959
neelpatel654 <nealpatel2004@gmail.com>
2025-03-20 15:39:12 +0530

    second commit

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,14 +1,16 @@
 // Invoke Functions Call on Document Loaded
-document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   hljs.highlightAll();
+// });
 
+let alertWrapper = document.querySelector(".alert");
+let alertClose = document.querySelector(".alert__close");
+console.log("alertWarapper", alertWrapper, "alertClose", alertClose);
 
-let alertWrapper = document.querySelector('.alert')
-let alertClose = document.querySelector('.alert__close')
-
-if (alertWrapper) {
-  alertClose.addEventListener('click', () =>
-    alertWrapper.style.display = 'none'
-  )
+if (alertClose) {
+  console.log("Alert Wrapper Clicked");
+  alertClose.addEventListener(
+    "click",
+    () => (alertWrapper.style.display = "none")
+  );
 }
--- users/views.py
+++ users/views.py
@@ -4,13 +4,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 
 # Create your views here.
 
 def loginUser(request):
     page = 'login'
     if request.user.is_authenticated:
+        messages.info(request,'User was logged in')
         return redirect('profiles')
 
     if request.method== 'POST':
@@ -51,7 +52,7 @@ def registerUser(request):
             messages.success(request,'User account was created!')
 
             login(request,user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.success(request,"An error occurred during registration") 
     
@@ -69,3 +70,71 @@ def userProfile(request,pk):
     otherSkills = profile.skill_set.filter(description="")
     context = {'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
     return render(request,'users/user-profile.html',context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+   
+
+    context = {'profile':profile,'skills':skills,'projects':projects}
+    return render(request,'users/account.html',context)
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method=="POST":
+        form=ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+    context = {'form':form}
+    return render(request,"users/profile_form.html",context)
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile = request.user.profile
+    form = SkillForm()
+
+    if request.method == "POST":
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            skill = form.save(commit=False)
+            skill.owner = profile
+            skill.save()
+            messages.success(request,'Skill was Added successfully')
+        return redirect('account')
+    context = {'form':form}
+    return render(request,'users/skill_form.html',context)
+
+
+@login_required(login_url='login')
+def updateSkill(request,pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    form = SkillForm(instance=skill)
+
+    if request.method == "POST":
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Skill was Updated successfully')
+        return redirect('account')
+    context = {'form':form}
+    return render(request,'users/skill_form.html',context)
+
+
+def deleteSkill(request,pk):
+    profile = request.user.profile 
+    skill = profile.skill_set.get(id=pk)
+    if request.method == 'POST':
+        skill.delete()
+        messages.success(request,'Skill was Deleted successfully')
+        return redirect('account')
+    context = {'object':skill}
+    return render(request, 'delete_template.html',context)
